f4f3dfbb4056de2f412530c354c8c596b16c4fab
mathias-villena <mathias.villena@tecsup.edu.pe>
2025-04-02 18:08:21 -0500

    ga

--- quiz_app_Lab03/src/quiz/urls.py
+++ quiz_app_Lab03/src/quiz/urls.py
@@ -6,5 +6,7 @@ urlpatterns = [
     path('exam/<int:exam_id>/', views.exam_detail, name='exam_detail'),
     path('exam/create/', views.exam_create, name='exam_create'),
     path('exam/<int:exam_id>/question/add/', views.question_create, name='question_create'),
-    path('exam/<int:exam_id>/play/', views.exam_play, name='exam_play'),
+    path('exam/<int:exam_id>/play/', views.exam_play, name='exam_play'),  
+    path('question/<int:question_id>/update/', views.question_update, name='question_update'),
+    path('question/<int:question_id>/delete/', views.question_delete, name='question_delete'),
 ]
