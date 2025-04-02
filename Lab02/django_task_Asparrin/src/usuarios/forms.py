6d737d394fd157d3c0a398a78de767c3acc5d74b
SujetoDelta-XXI <carlos.asparrin@tecsup.edu.pe>
2025-04-02 15:38:40 -0500

    Quiz

--- Lab02/django_task_Asparrin/src/usuarios/forms.py
+++ Lab02/django_task_Asparrin/src/usuarios/forms.py
@@ -1,11 +1,11 @@
 from django import forms
 from .models import Usuarios
 
-class TaskForm(forms.ModelForm):
+class UsuariosForm(forms.ModelForm):
     class Meta:
         model = Usuarios
-        fields = ['name', 'profesion', 'special_date', 'estado']
+        fields = ['name', 'profesion', 'special_day', 'estado']
         widgets = {
-            'special_date': forms.DateInput(attrs={'type': 'date'}),
+            'special_day': forms.DateInput(attrs={'type': 'date'}),
             'profesion': forms.Textarea(attrs={'rows': 4}),
         }
