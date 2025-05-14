4c513cd30415c07562a5a5b910325fe2b49106a0
Angeela-Lopeez <angela.lopez@tecsup.edu.pe>
2025-05-14 15:52:37 -0500

    base

---
+++ Lab09/src/App.jsx
@@ -0,0 +1,32 @@
+import './App.css'
+import SerieComponent from './components/SerieComponent'
+
+function App() {
+  const series = [
+    {cod:1, nom:"Friends", cat:"Comedy", img:"friends.png"},
+    {cod:2, nom:"Law & Order", cat:"Drama", img:"law-and-order.png"},
+    {cod:3, nom:"The Big Bang Theory", cat:"Comedy", img:"the-big-bang-theory.png"},
+    {cod:4, nom:"Stranger Things", cat:"Terror", img:"stranger-things.png"},
+    {cod:5, nom:"Dr. House", cat:"Drama", img:"dr-house.png"},
+    {cod:6, nom:"The X-Files", cat:"Drama", img:"the-x-files.png"}
+  ];
+  return (
+    <div classNameName="container mt-3">
+      <h1 classNameName="border-bottom pb-3 mb-3">Series</h1>
+      <div classNameName="row">
+        {series.map((serie) => (
+          <div key={serie.codigo} classNameName="col-md-4 mb-3">
+            <SerieComponent
+              codigo={serie.cod}
+              nombre={serie.nom}
+              genero={serie.cat}
+              imagen={serie.img}
+            />
+          </div>
+        ))}
+      </div>
+    </div>
+  );
+}
+
+export default App;
