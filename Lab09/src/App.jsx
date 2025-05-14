b6928ee48d859e908aff026fbf3df749a938d4a4
SujetoDelta-XXI <carlos.asparrin@tecsup.edu.pe>
2025-05-14 16:28:32 -0500

    a

--- Lab09/src/App.jsx
+++ Lab09/src/App.jsx
@@ -1,7 +1,7 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import SerieComponent from './components/SerieComponent';
-import Header from './components/Header'
-import DescripcionComponent from './components/DescripcionComponent'
+import Header from './components/Header';
+import DescripcionComponent from './components/DescripcionComponent';
 
 function App() {
   const series = [
