9c5883d1e22614fe87fdee11c29ff4675b9fde2e
mathias-villena <mathias.villena@tecsup.edu.pe>
2025-05-14 16:41:34 -0500

    footer xd

--- Lab09/src/components/Footer.jsx
+++ Lab09/src/components/Footer.jsx
@@ -3,7 +3,7 @@ import './Footer.css';
 function Footer() {
   return (
     <footer>
-      <p>© 2024 Grupo 03 - Laboratorio S09</p>
+      <p>© 2024 Grupo 02 - Laboratorio S09 - El bajemendi</p>
     </footer>
   );
 }
