6bd60179d725a1833e0434f78e44eaab3209e528
SujetoDelta-XXI <carlos.asparrin@tecsup.edu.pe>
2025-05-14 16:27:43 -0500

    Index - Cambios - Asparrin

--- Lab09/src/App.jsx
+++ Lab09/src/App.jsx
@@ -1,36 +1,42 @@
-import './App.css'
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import SerieComponent from './components/SerieComponent';
 import Header from './components/Header'
-import SerieComponent from './components/SerieComponent'
+import DescripcionComponent from './components/DescripcionComponent'
 
 function App() {
   const series = [
-    {cod:1, nom:"Friends", cat:"Comedy", img:"friends.png"},
-    {cod:2, nom:"Law & Order", cat:"Drama", img:"law-and-order.png"},
-    {cod:3, nom:"The Big Bang Theory", cat:"Comedy", img:"the-big-bang-theory.png"},
-    {cod:4, nom:"Stranger Things", cat:"Terror", img:"stranger-things.png"},
-    {cod:5, nom:"Dr. House", cat:"Drama", img:"dr-house.png"},
-    {cod:6, nom:"The X-Files", cat:"Drama", img:"the-x-files.png"}
+    {cod:1, nom:"Friends", cat:"Comedia", img:"Friends"},
+    {cod:2, nom:"Law & Order", cat:"Drama", img:"Law+%26+Order"},
+    {cod:3, nom:"The Big Bang Theory", cat:"Comedia", img:"The+Big+Bang+Theory"},
+    {cod:4, nom:"Stranger Things", cat:"Terror", img:"Stranger+Things"},
+    {cod:5, nom:"Dr. House", cat:"Drama", img:"Dr.+House"},
+    {cod:6, nom:"The X-Files", cat:"Drama", img:"The+X-Files"}
   ];
 
   return (
-    <>
+    <BrowserRouter>
       <Header />
-      <div className="container mt-3">
-        <h1 className="border-bottom pb-3 mb-3">Series</h1>
-        <div className="row">
-          {series.map((serie) => (
-            <div key={serie.cod} className="col-md-4 mb-3">
-              <SerieComponent
-                codigo={serie.cod}
-                nombre={serie.nom}
-                genero={serie.cat}
-                imagen={serie.img}
-              />
+      <Routes>
+        <Route path="/" element={
+          <div className="container mt-3">
+            <h1 className="border-bottom pb-3 mb-3">Series</h1>
+            <div className="row">
+              {series.map((serie) => (
+                <div key={serie.cod} className="col-md-4 mb-3">
+                  <SerieComponent
+                    codigo={serie.cod}
+                    nombre={serie.nom}
+                    genero={serie.cat}
+                    imagen={serie.img}
+                  />
+                </div>
+              ))}
             </div>
-          ))}
-        </div>
-      </div>
-    </>
+          </div>
+        } />
+        <Route path="/descripcion/:nombre/:genero" element={<DescripcionComponent />} />
+      </Routes>
+    </BrowserRouter>
   );
 }
 
--- Lab09/src/components/SerieComponent.jsx
+++ Lab09/src/components/SerieComponent.jsx
@@ -1,18 +1,31 @@
+import { useNavigate } from "react-router-dom";
+
 function SerieComponent(props) {
+  const navigate = useNavigate();
+
+  const handleDescripcion = () => {
+    // Navega a la ruta de descripción con los datos de la serie
+    navigate(`/descripcion/${encodeURIComponent(props.nombre)}/${encodeURIComponent(props.genero)}`);
+  };
+
   return (
-    <div className="card">
+    <div className="card h-100 w-100">
       <img 
         className="card-img-top" 
         src={"https://dummyimage.com/400x250/000/fff&text=" + props.imagen}
-        alt="img" 
+        alt={props.nombre} 
       />
-      <div className="card-body">
+      <div className="card-body d-flex flex-column">
         <h5 className="card-title">{props.nombre}</h5>
         <p className="card-text">{props.genero}</p>
-        <button className="btn btn-primary">Description</button>
+        <div className="mt-auto">
+          <button className="btn btn-primary w-100" onClick={handleDescripcion}>
+            Descripción
+          </button>
+        </div>
       </div>
     </div>
-  )
+  );
 }
 
-export default SerieComponent
+export default SerieComponent;
