d17632bea85aec9042615282e355edc2b996648a
SujetoDelta-XXI <carlos.asparrin@tecsup.edu.pe>
2025-05-21 16:03:36 -0500

    a

--- lab10/src/components/SerieComponent.jsx
+++ lab10/src/components/SerieComponent.jsx
@@ -1,27 +1,46 @@
 import { useNavigate } from "react-router-dom";
 
+import friendsImg from '../assets/friends.png';
+import lawAndOrderImg from '../assets/law-and-order.png';
+import bigBangImg from '../assets/the-big-bang-theory.png';
+import strangerThingsImg from '../assets/stranger-things.png';
+import drHouseImg from '../assets/dr-house.png';
+import xFilesImg from '../assets/the-x-files.png';
+
+const imagesMap = {
+  'friends.png': friendsImg,
+  'law-and-order.png': lawAndOrderImg,
+  'the-big-bang-theory.png': bigBangImg,
+  'stranger-things.png': strangerThingsImg,
+  'dr-house.png': drHouseImg,
+  'the-x-files.png': xFilesImg,
+};
+
 function SerieComponent(props) {
     const navigate = useNavigate();
 
     const gotoUrl = (codigo) => {
-        navigate("/series/edit/"+codigo);
-    }
+        navigate("/series/edit/" + codigo);
+    };
+
+    const imagenSrc = imagesMap[props.imagen] || 'https://via.placeholder.com/400x250?text=No+Image';
+
     return (
         <div className="card">
             <img 
                 className="card-img-top" 
-                src={"https://dummyimage.com/400x250/000/fff&text="+props.imagen} 
-                alt="img" />
+                src={imagenSrc} 
+                alt={props.nombre} />
             <div className="card-body">
                 <h5 className="card-title">{props.nombre}</h5>
                 <p className="card-text">{props.categoria}</p>
                 <div className="d-flex justify-content-between">
-                    <button onClick={()=>gotoUrl(props.codigo)} className="btn btn-secondary">Editar</button> 
+                    <button onClick={() => gotoUrl(props.codigo)} className="btn btn-secondary">Editar</button> 
                     <button className="btn btn-danger">Eliminar</button>
                 </div>
             </div>
         </div>
-    )
+    );
 }
-  
-export default SerieComponent
+
+export default SerieComponent;
--- lab10/src/pages/SeriePage.jsx
+++ lab10/src/pages/SeriePage.jsx
@@ -6,7 +6,7 @@ function SeriePage(){
     const series = [
         {cod:1, nom:"Friends", cat:"Comedy", img:"friends.png"},
         {cod:2, nom:"Law & Order", cat:"Drama", img:"law-and-order.png"},
-        {cod:3, nom:"The Big Bang Theory", cat:"Comedy", img:"the-big-bang.png"},
+        {cod:3, nom:"The Big Bang Theory", cat:"Comedy", img:"the-big-bang-theory.png"},
         {cod:4, nom:"Stranger Things", cat:"Horror", img:"stranger-things.png"},
         {cod:5, nom:"Dr. House", cat:"Drama", img:"dr-house.png"},
         {cod:6, nom:"The X-Files", cat:"Drama", img:"the-x-files.png"},
